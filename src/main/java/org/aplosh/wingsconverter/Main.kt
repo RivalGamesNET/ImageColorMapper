c2a0f52c7d4b0c741c763029711a44ff71a63951
Aplosh <business.alfieyt@gmail.com>
2023-08-11 03:25:29 +0100

    Added documentation to the method

--- src/main/java/org/aplosh/wingsconverter/Main.kt
+++ src/main/java/org/aplosh/wingsconverter/Main.kt
@@ -9,29 +9,54 @@ object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val img = ImageIO.read(File("E:\\Code\\BlockGame\\WingsConverter\\wings.png"))
+        // Read an image from a file
+        val img = ImageIO.read(File(""))
+
+        // AtomicInteger used to generate unique color IDs
         val colorIndex = AtomicInteger()
+
+        // MutableMap to store color-to-ID mappings
         val indexByColor: MutableMap<Color, String> = HashMap()
+
+        // Initialize the map with a default color ID
         indexByColor[Color(0, 0, 0)] = "-1"
+
+        // Print the start of a JSON-like array
         println("{")
+
+        // Iterate through each pixel in the image
         for (y in 0 until img.height) {
+            // Start printing an inner array
             print("    {")
+
             for (x in 0 until img.width) {
+                // Get the color of the current pixel
                 val color = Color(img.getRGB(x, y))
-                val id = indexByColor.computeIfAbsent(color) { c: Color? -> colorIndex.addAndGet(11).toString() }
+
+                // Compute or retrieve the color's ID, generating a new one if necessary
+                val id = indexByColor.computeIfAbsent(color) { _: Color? -> colorIndex.addAndGet(11).toString() }
+
+                // Print the color's ID
                 print("\"" + id + "\"")
+
+                // Add a comma if not the last pixel in the row
                 if (x != img.width - 1) {
                     print(", ")
                 }
             }
 
+            // Close the inner array for the current row
             print("}")
+
+            // Add a comma if not the last row
             if (y != img.height - 1) {
                 println(",")
             } else {
                 println()
             }
         }
+
+        // Print the end of the JSON-like array
         println("}")
     }
 }
