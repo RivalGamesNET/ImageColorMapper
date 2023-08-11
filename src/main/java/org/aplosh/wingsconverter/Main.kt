af6a080676d9c2f7559f29baa91c5a10a13fc87c
Aplosh <business.alfieyt@gmail.com>
2023-08-11 03:21:20 +0100

    Kotlin

--- src/main/java/org/aplosh/wingsconverter/Main.kt
+++ src/main/java/org/aplosh/wingsconverter/Main.kt
@@ -1,41 +1,37 @@
-package org.aplosh.wingsconverter;
+package org.aplosh.wingsconverter
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.awt.Color
+import java.io.File
+import java.util.concurrent.atomic.AtomicInteger
+import javax.imageio.ImageIO
 
-public class Main {
+object Main {
 
-    public static void main(String[] args) throws Exception {
-        BufferedImage img = ImageIO.read(new File("file path here"));
-        AtomicInteger colorIndex = new AtomicInteger();
-        Map<Color, String> indexByColor = new HashMap<>();
-
-        indexByColor.put(new Color(0, 0, 0), "-1");
-
-        System.out.println("{");
-        for (int y = 0; y < img.getHeight(); ++y) {
-            System.out.print("    {");
-            for (int x = 0; x < img.getWidth(); ++x) {
-                Color color = new Color(img.getRGB(x, y));
-                String id = indexByColor.computeIfAbsent(color, c -> String.valueOf(colorIndex.addAndGet(11)));
-                System.out.print("\"" + id + "\"");
-                if (x != img.getWidth() - 1) {
-                    System.out.print(", ");
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val img = ImageIO.read(File("E:\\Code\\BlockGame\\WingsConverter\\wings.png"))
+        val colorIndex = AtomicInteger()
+        val indexByColor: MutableMap<Color, String> = HashMap()
+        indexByColor[Color(0, 0, 0)] = "-1"
+        println("{")
+        for (y in 0 until img.height) {
+            print("    {")
+            for (x in 0 until img.width) {
+                val color = Color(img.getRGB(x, y))
+                val id = indexByColor.computeIfAbsent(color) { c: Color? -> colorIndex.addAndGet(11).toString() }
+                print("\"" + id + "\"")
+                if (x != img.width - 1) {
+                    print(", ")
                 }
             }
 
-            System.out.print("}");
-            if (y != img.getHeight() - 1) {
-                System.out.println(",");
+            print("}")
+            if (y != img.height - 1) {
+                println(",")
             } else {
-                System.out.println();
+                println()
             }
         }
-        System.out.println("}");
+        println("}")
     }
 }
